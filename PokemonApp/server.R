7014e03965bf2e847a1b36f2b1bfc007977ed1d6
AnchSaxena <asaxena4@ncsu.edu>
2019-07-23 23:08:59 -0400

    Basic stat updated

--- PokemonApp/server.R
+++ PokemonApp/server.R
@@ -9,16 +9,30 @@ library(corrplot)
 library(readr)
 library(ggplot2)
 library(stringr)
-library(caret)
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+library(readr)
 library(DT)
+library(caret)
+
+#read data/clean it
+pokemonData <- read_csv("C:\\Users\\dbhat\\Desktop\\ST558\\FinalProject-ST558\\Pokemon.csv")
+
+pokemonData$Legendary <- as.factor(pokemonData$Legendary)
+set.seed(1)
+train <- sample(1:nrow(pokemonData), size = nrow(pokemonData)*0.8)
+test <- dplyr::setdiff(1:nrow(pokemonData), train)
+pokemonDataTrain <- pokemonData[train, ]
+pokemonDataTest <- pokemonData[test, ]
 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
     #Reading in file
     getData <- reactive({
-        #read.csv("C:\\Users\\dbhat\\Desktop\\ST558\\FinalProject-ST558\\Pokemon.csv")
-       dt<- mtcars
+        read.csv("C:\\Users\\dbhat\\Desktop\\ST558\\FinalProject-ST558\\Pokemon.csv")
+       #dt<- mtcars
     })
 
     #######################################################################
@@ -31,18 +45,78 @@ shinyServer(function(input, output, session) {
         summary(getData())
     })
     
-    #Corr plot
-    output$corPlot <- renderPlot({
-        dat <- data.matrix(getData())
-        corrplot(dat)
+    #Printing basic statistic
+    output$genSummary <- renderPrint({
+        df <- getData()
+        df2 <- df %>% group_by(df$Generation) %>% 
+            summary(avg_speed = mean(Speed), avg_hp = mean(HitPoints), 
+                    avg_attack = mean(Attack), 
+                    avg_defense = mean(Defense), 
+                    avg_sp_attack = mean(SpecialAttack), 
+                    avg_sp_defense = mean(SpecialDefense))
+        df2
+    })
+    
+    #Plotting variables
+    output$pairs <- renderPlot({
+        df <- getData()
+        ggplot(df, aes(df$Attack, df$Defense)) + 
+            geom_point(aes(color = df$Generation)) +
+            theme_bw() + labs(title="Scatterplot") + 
+            facet_wrap( ~ df$Generation)
+    })
+    
+    
+    #Corr plot1
+    output$corPlot1 <- renderPlot({
+        df <- getData()
+        Correlation <- cor(df[,5:12], method = "spearman")
+        #plotting correlation
+        corrplot(Correlation,type="lower")
+    })
+    
+    #Corr plot2
+    output$corPlot2 <- renderPlot({
+        df <- getData()
+        g <- ggplot(df, aes(df$Attack, df$Defense)) 
+        g + geom_point(aes(df$Attack, df$Defense, color = df$Legendary)) +
+            theme_bw() + labs(title="Scatterplot")
     })
         
 
     #######################################################################
     #Data Tab
     #######################################################################
-    output$table <- renderTable({
-        getData()
+    output$table1 <- renderDataTable({
+        datatable(getData()[,2:13])
+    })
+    #######################################################################
+    #kNN Model
+    #######################################################################
+    observe({
+        set.seed(1)
+        knn.pred <- knn(data.frame(pokemonDataTrain[,input$checkGroup]),
+                        data.frame(pokemonDataTrain[,input$checkGroup]),
+                        Legendary, k = input$k)
+        
+        
+        output$value <- renderText({ paste("Classification Error = ",ce(test.Y,knn.pred)) })
+        output$confusionMatrix <- renderDataTable({
+            # modify this to show title - confusion matrix
+            # /false positive/positive false negative/negative
+            true.positive    <- sum(knn.pred == "positive" & test.Y == "positive")
+            false.positive   <- sum(knn.pred == "negative" & test.Y == "positive")
+            true.negative    <- sum(knn.pred == "negative" & test.Y == "negative")
+            false.negative   <- sum(knn.pred == "positive" & test.Y == "negative")
+            row.names <- c("Prediction - FALSE", "Prediction - TRUE" )
+            col.names <- c("Reference - FALSE", "Reference - TRUE")
+            cbind(Outcome = row.names, as.data.frame(matrix( 
+                c(true.negative, false.negative, false.positive, true.positive) ,
+                nrow = 2, ncol = 2, dimnames = list(row.names, col.names))))
+        }, options = table.settings
+        )
+        
     })
 })
+
 
--- PokemonApp/ui.R
+++ PokemonApp/ui.R
@@ -29,103 +29,47 @@ shinyUI(
 ),
         dashboardBody(
     tabItems(
-        tabItem(tabName = "Introduction",
-                h2("Welcome to Data Science with Pokemon!"),
-                img(src="C:\\Users\\dbhat\\Desktop\\ST558\\FinalProject-ST558/image.jpg", width = '100%'),
-                br(),
-                h3("Introduction"),
-                br(),
-                h4("We'll be using pokemon dataset to walk through basics of Data Science."),
-                br(),
-                h3("Data"),
-                h4("The dataset contains 13 attributes with 800 samples and my aim will be finding whether a given pokemon is legendary or not."),
-                br(),
-                h4("The Attributes are:"),
-                br(),
-                h4("#: ID for each pokemon"),
-                h4("Name: Name of each pokemon"),
-                h4("Type 1: Each pokemon has a type, this determines weakness/resistance to attacks"),
-                h4("Type 2: Some pokemon are dual type and have 2"),
-                h4("Total: sum of all stats that come after this, a general guide to how strong a pokemon is"),
-                h4("HP: hit points, or health, defines how much damage a pokemon can withstand before fainting"),
-                h4("Attack: the base modifier for normal attacks (eg. Scratch, Punch)"),
-                h4("Defense: the base damage resistance against normal attacks"),
-                h4("SP Atk: special attack, the base modifier for special attacks (e.g. fire blast, bubble beam)"),
-                h4("SP Def: the base damage resistance against special attacks"),
-                h4("Speed: determines which pokemon attacks first each round")
-                
-        ),
-        tabItem(tabName = "graphSummary",
+        tabItem(tabName = "dataSet",
                 fluidRow(
-                    box(
-                    title = "Histogram", width = 6, solidHeader = TRUE,
-                    collapsible = TRUE, status = "primary",
-                    plotOutput("distPlot", height = 250)
-                    ),
-                    box(
-                    title = "Scatter Plot", width = 6, solidHeader = TRUE,
-                    collapsible = TRUE, status = "primary",
-                    plotOutput("distPlot", height = 250)
-                    )
+                div(style = 'overflow-x: scroll', dataTableOutput("table1"))
+                
                 )
         ),
         tabItem(tabName = "numSummary",
                 fluidRow(
-                    h3("Basic statistics"),
-                    verbatimTextOutput("basicStat"),
-                    br(),
                     box(
-                        title = "Scatter Plot", width = 6, solidHeader = TRUE,
-                        collapsible = TRUE, status = "primary",
-                        plotOutput("corPlot", height = 250)
-                    )
-                    
-                )
-        ),
-        tabItem(tabName = "unsuper",
-                fluidRow(
-                    box(
-                        title = "Select the PCs to plot", width = 6,
-                        solidHeader = TRUE, collapsible = TRUE, 
-                        status = "warning",
-                        uiOutput("the_pcs_to_plot_x"),
-                        uiOutput("the_pcs_to_plot_y")
+                    title = "Basic statistics",
+                    solidHeader = TRUE,  width = 12,
+                    collapsible = TRUE, status = "primary",
+                    verbatimTextOutput("basicStat")
                     ),
                     box(
-                        title = "BiPlot", width = 6, solidHeader = TRUE,
+                        title = "Summary by Generation",
+                        solidHeader = TRUE,  width = 12,
                         collapsible = TRUE, status = "primary",
-                        plotOutput("distPlot", height = 250)
+                        verbatimTextOutput("genSummary")
                     )
                 )
         ),
-        tabItem(tabName = "super",
+        tabItem(tabName = "graphSummary",
                 fluidRow(
                     box(
-                        title = "Select the PCs to plot", width = 6,
-                        solidHeader = TRUE, collapsible = TRUE, 
-                        status = "warning",
-                        uiOutput("varselect2"),
-                        uiOutput("the_pcs_to_plot_y")
+                        title = "Scatter Plot", width = 6, 
+                        solidHeader = TRUE,
+                        collapsible = TRUE, status = "primary",
+                        plotOutput("corPlot1", height = 250)
                     ),
                     box(
-                        title = "BiPlot", width = 6, solidHeader = TRUE,
+                        title = "Scatter Plot", width = 6, 
+                        solidHeader = TRUE,
                         collapsible = TRUE, status = "primary",
-                        plotOutput("distPlot", height = 250)
+                        plotOutput("corPlot2", height = 250)
+                    ),
+                    box(
+                        title = "Attack v/s Defense", width = 8, 
+                        solidHeader = TRUE,
+                        collapsible = TRUE, status = "primary",
+                        plotOutput("pairs", height = 250)
                     )
-                )
-        ),
-        tabItem(tabName = "dataSet",
-                fluidRow(
-                    box(title = "Dataset used", color = "blue", 
-                        ribbon = FALSE,
-                        title_side = "top left", width = 14,
-                        
-                            tableOutput("table")
-                            
-                        )
-                    )
-                
-        )
-    )
-    )
-))
+                ))
+))))
